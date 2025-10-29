fa3bda5998de2e14f2b4803e0f5221acef17e556
PraNAtaP <pranatapu08@gmail.com>
2025-10-29 16:17:09 +0700

    Percobaan 1

---
+++ AbstractClassDemo.java
@@ -0,0 +1,15 @@
+public class AbstractClassDemo {
+    public static void main(String[] args) {
+        Hewan hewan1 = new Hewan(10, "Rumput", "Savana");
+        hewan1.cetakInfo();
+        hewan1.bergerak();
+        hewan1.bernapas();
+
+        System.out.println("\n--- Data Lebah ---");
+        Lebah lebah1 = new Lebah("Ratu", 0.05, "Nektar", "Hutan");
+        lebah1.cetakInfo();
+        lebah1.bergerak(); 
+        lebah1.bernapas(); 
+    }
+}
+
